687ddf3004aaab9822168857d2b763988e657f29
Dream-OvO <dreaming_life@mail.ru>
2018-10-31 18:40:07 +0800

    Created page

---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+
+
+const mainSlide = document.getElementById('main')
+
+function setImageSlide(imgName) {
+  mainSlide.setAttribute(
+    'style',
+    `background-image: url(./assets/img/${imgName}.jpg);`
+  )
+}
